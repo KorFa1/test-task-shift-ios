e253c020efaf9535422373957bcd871ad58fde4c
Kirill <sofrin2013@gmail.com>
2025-07-04 14:45:59 +0300

    Initial commit

---
+++ TestTaskShift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestTaskShift
+//
+//  Created by Кирилл Софрин on 04.07.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemRed
+    }
+
+
+}
+
